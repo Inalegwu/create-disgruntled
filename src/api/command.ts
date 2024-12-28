a9fc42e432889dd42c2f0f53f78d8849b9d139ae
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2024-12-28 23:07:55 +0100

    [feature] chose variants when creating desktop apps

--- src/api/command.ts
+++ src/api/command.ts
@@ -1,33 +1,38 @@
-import { CommandError } from '@/error';
-import { Args, Command } from '@effect/cli';
-import { $ } from 'bun';
-import { Effect } from 'effect';
+import { CommandError } from "@/error";
+import { Args, Command } from "@effect/cli";
+import { FileSystem } from "@effect/platform";
+import { $ } from "bun";
+import { Effect } from "effect";
 
-const name = Args.text({ name: 'path' }).pipe(Args.withDefault('my_app'));
+const name = Args.text({ name: "path" }).pipe(Args.withDefault("my_app"));
 
-export const api = Command.make('api', { name }, ({ name }) =>
-  Effect.gen(function* () {
-    yield* Effect.logInfo(`Creating API Project @: ${name}`);
+export const api = Command.make(
+  "api",
+  { name },
+  ({ name }) =>
+    Effect.gen(function* () {
+      const fs = yield* FileSystem.FileSystem;
+      yield* Effect.logInfo(`Creating API Project @: ${name}`);
 
-    yield* Effect.tryPromise({
-      try: async () => {
-        const lines =
-          $`git clone https://github.com/Inalegwu/Martini ${name}`.lines();
+      yield* Effect.tryPromise({
+        try: async () => {
+          const lines = $`git clone https://github.com/Inalegwu/Martini ${name}`
+            .lines();
 
-        for await (const line of lines) {
-          console.log(line);
-        }
-      },
-      catch: (cause) => new CommandError({ cause }),
-    });
-  }).pipe(
-    Effect.catchTags({
-      'command-error': (error) =>
-        Effect.gen(function* () {
-          yield* Effect.logError(
-            `Error Occurred Creating Your API App ${error.cause}`,
-          );
-        }),
-    }),
-  ),
+          for await (const line of lines) {
+            console.log(line);
+          }
+        },
+        catch: (cause) => new CommandError({ cause }),
+      });
+    }).pipe(
+      Effect.catchTags({
+        "command-error": (error) =>
+          Effect.gen(function* () {
+            yield* Effect.logError(
+              `Error Occurred Creating Your API App ${error.cause}`,
+            );
+          }),
+      }),
+    ),
 );
--- src/desktop/command.ts
+++ src/desktop/command.ts
@@ -1,18 +1,39 @@
-import { CommandError } from '@/error';
-import { Args, Command } from '@effect/cli';
-import { $ } from 'bun';
-import { Effect } from 'effect';
+import { CommandError } from "@/error";
+import { Args, Command, Options } from "@effect/cli";
+import { $ } from "bun";
+import { Effect, Option } from "effect";
 
-const name = Args.text({ name: 'path' }).pipe(Args.withDefault('my_app'));
+const name = Args.text({ name: "path" }).pipe(Args.withDefault("my_app"));
+const framework = Options.choice("variant", ["reactjs", "solidjs"]).pipe(
+  Options.withAlias("v"),
+  Options.optional,
+);
 
-export const desktop = Command.make('desktop', { name }, ({ name }) =>
-  Effect.gen(function* () {
-    yield* Effect.logInfo(`Creating Desktop Project @: ${name}`);
+export const desktop = Command.make(
+  "desktop",
+  { name, framework },
+  ({ name, framework }) =>
+    Effect.gen(function* () {
+      yield* Option.match(framework, {
+        onNone: () => handleCreate("solidjs", name),
+        onSome: (variant) => handleCreate(variant, name),
+      });
+    }),
+);
+
+function handleCreate(framework: "reactjs" | "solidjs", name: string) {
+  return Effect.gen(function* () {
+    yield* Effect.logInfo(
+      `Creating Desktop Project @: ${name} with ${
+        framework === "reactjs" ? "React JS" : "Solid JS"
+      }`,
+    );
 
     yield* Effect.tryPromise({
       try: async () => {
         const lines =
-          $`git clone https://github.com/Inalegwu/ElectroStatic ${name}`.lines();
+          $`git clone https://github.com/Inalegwu/${framework==="reactjs"?"ElectroStatic":"Solidtron"} ${name}`
+            .lines();
 
         for await (const line of lines) {
           console.log(line);
@@ -22,12 +43,14 @@ export const desktop = Command.make('desktop', { name }, ({ name }) =>
     });
   }).pipe(
     Effect.catchTags({
-      'command-error': (error) =>
+      "command-error": (error) =>
         Effect.gen(function* () {
           yield* Effect.logError(
-            `An error occurred attempting to create your desktop project ${String(error.cause).includes('128') ? 'NetworkError' : ''}`,
+            `An error occurred attempting to create your desktop project ${
+              String(error.cause).includes("128") ? "NetworkError" : ""
+            }`,
           );
         }),
     }),
-  ),
-);
+  );
+}
