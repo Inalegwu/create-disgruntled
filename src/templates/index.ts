7a4ea775ffc76a21c4fe4f9c779df705873468e3
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2025-02-17 23:56:20 +0100

    [chore] clean up and streamline

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,5 @@
 import { FileSystem, Path } from '@effect/platform';
-import { Array, Data, Effect, Match, Option, Schema } from 'effect';
+import { Array, Data, Effect, Option, Schema } from 'effect';
 import child_proc from 'node:child_process';
 import { username } from 'username';
 import { type TTemplateSchema, TemplateSchema } from './schema';
@@ -27,15 +27,20 @@ export class Init extends Effect.Service<Init>()(
         name: Option.Option<string>,
         opts: Options,
       ) {
+        // make sure all our options aren't none by the time we use them
+        // by resolving them or providing a default so the input doesn't
+        // worry about passing a default
         const cmd = Option.getOrElse(opts.manager, () => 'npm' as const);
         const projectName = Option.getOrElse(name, () => 'my-project');
         const projectDir = path_.join(process.cwd(), projectName);
-        const template_ = Option.getOrElse(
+        const template = Option.getOrElse(
           opts.template,
           () => 'default' as const,
         );
 
-        // TODO: troubleshoot this, for some reason the directory already exists
+        // ! this one is tricky. by default, fs_.exists() returns whether
+        // ! or NOT the project exists, if it does exist, true. doesn't, false,
+        // ! so we need to make sure that doesn't exist is true instead of false
         if (!fs_.exists(projectDir)) {
           yield* Effect.logError(`${projectDir} Already Exists`);
           yield* Effect.die(
@@ -46,25 +51,20 @@ export class Init extends Effect.Service<Init>()(
           );
         }
 
-        yield* fs_.makeDirectory(projectDir, {
-          recursive: true,
-        });
+        // create the directory for the project based on the
+        // resolved project directory
+        yield* fs_.makeDirectory(projectDir);
 
-        const template = yield* getTemplate({
-          template: template_,
-          command: opts.command,
-        });
-
-        const packageManager = yield* getPackageManager({
-          command: opts.command,
-          manager: cmd,
-        });
-
+        // get the template directory based on the
+        // command making the request and the template
+        // being requested
         const templateDir = path_.resolve(
           __dirname,
           `../../templates/${opts.command}/${template}`,
         );
 
+        // if the template doesn't exist
+        // bail out
         if (!fs_.exists(templateDir)) {
           yield* Effect.die(
             new InitError({
@@ -73,15 +73,17 @@ export class Init extends Effect.Service<Init>()(
             }),
           );
         }
-
+        // load in the templates' package.json
         const templateJson = yield* fs_.readFileString(
           path_.join(templateDir, 'template.json'),
         );
 
+        // and convert to a known format
         const schema_ = yield* Schema.decodeUnknown(TemplateSchema)(
           JSON.parse(templateJson),
         );
 
+        // append our new package data to the old
         const newPackage = {
           ...schema_,
           name: projectName,
@@ -92,16 +94,23 @@ export class Init extends Effect.Service<Init>()(
           },
         } satisfies TTemplateSchema;
 
+        // copy the template
         yield* fs_.copy(templateDir, projectDir, {
           overwrite: false,
           preserveTimestamps: false,
         });
 
-        yield* fs_.rename(
-          path_.join(projectDir, 'template.json'),
+        // create a package.json file with our
+        // new package content
+        yield* fs_.writeFileString(
           path_.join(projectDir, 'package.json'),
+          JSON.stringify(newPackage, null, '\t'),
         );
 
+        // remove the template's package.json definition
+        yield* fs_.remove(path_.join(projectDir, 'template.json'));
+
+        // rename the gitignore file
         yield* fs_.rename(
           path_.join(projectDir, 'gitignore'),
           path_.join(projectDir, '.gitignore'),
@@ -110,21 +119,23 @@ export class Init extends Effect.Service<Init>()(
         const deps = Array.fromRecord(newPackage.dependencies);
         const devDeps = Array.fromRecord(newPackage.devDependencies);
 
+        // install all dependencies within the project
         const installDeps = Effect.try(() =>
           child_proc.exec(`${cmd} add ${deps.join(' ')}`, {
             cwd: projectDir,
           }),
-        );
+        ).pipe(Effect.orDie);
         const installDevDeps = Effect.try(() =>
           child_proc.exec(`${cmd} add -D ${devDeps.join(' ')}`, {
             cwd: projectDir,
           }),
-        );
+        ).pipe(Effect.orDie);
 
         yield* Effect.logInfo(
           `Installing Project Dependencies via ${cmd}...this might take a few minutes`,
         );
 
+        // execute all installations
         yield* Effect.all([installDeps, installDevDeps], {
           concurrency: 2,
         });
@@ -134,48 +145,3 @@ export class Init extends Effect.Service<Init>()(
     }),
   },
 ) {}
-
-const getPackageManager = (value: {
-  command: Options['command'];
-  manager: Option.Option.Value<Options['manager']>;
-}) =>
-  Match.value(value).pipe(
-    Match.when({ command: 'desktop', manager: 'pnpm' }, (_) => _.manager),
-    Match.when({ command: 'desktop', manager: 'npm' }, (_) => _.manager),
-    Match.when({ command: 'desktop', manager: 'yarn' }, (_) => _.manager),
-    // force the user to use pnpm...should probably give an error but, we'll see
-    Match.when({ command: 'desktop', manager: 'bun' }, () => 'pnpm' as const),
-    Match.when({ command: 'mobile', manager: 'bun' }, (_) => _.manager),
-    Match.when({ command: 'mobile', manager: 'npm' }, (_) => _.manager),
-    Match.when({ command: 'mobile', manager: 'pnpm' }, (_) => _.manager),
-    Match.when({ command: 'mobile', manager: 'yarn' }, (_) => _.manager),
-    Match.when({ command: 'web', manager: 'bun' }, (_) => _.manager),
-    Match.when({ command: 'web', manager: 'npm' }, (_) => _.manager),
-    Match.when({ command: 'web', manager: 'pnpm' }, (_) => _.manager),
-    Match.when({ command: 'web', manager: 'yarn' }, (_) => _.manager),
-    Match.when({ command: 'cli', manager: 'bun' }, (_) => _.manager),
-    Match.when({ command: 'cli', manager: 'npm' }, (_) => _.manager),
-    Match.when({ command: 'cli', manager: 'pnpm' }, (_) => _.manager),
-    Match.when({ command: 'cli', manager: 'yarn' }, (_) => _.manager),
-    Match.when({ command: 'api', manager: 'bun' }, (_) => _.manager),
-    Match.when({ command: 'api', manager: 'npm' }, (_) => _.manager),
-    Match.when({ command: 'api', manager: 'pnpm' }, (_) => _.manager),
-    // TODO had to remove one Match.when statement here, Match.option can't take
-    // more than 20 parameters
-    Match.option,
-  );
-
-const getTemplate = (value: {
-  template: Option.Option.Value<Options['template']> | 'default';
-  command: Options['command'];
-}) =>
-  Match.value(value).pipe(
-    Match.when({ command: 'desktop', template: 'reactjs' }, (_) => _.template),
-    Match.when({ command: 'desktop', template: 'solidjs' }, (_) => _.template),
-    Match.when({ command: 'mobile', template: 'default' }, (_) => _.template),
-    Match.when({ command: 'api', template: 'default' }, (_) => _.template),
-    Match.when({ command: 'cli', template: 'default' }, (_) => _.template),
-    Match.when({ command: 'web', template: 'nextjs' }, (_) => _.template),
-    Match.when({ command: 'web', template: 'remixjs' }, (_) => _.template),
-    Match.option,
-  );
